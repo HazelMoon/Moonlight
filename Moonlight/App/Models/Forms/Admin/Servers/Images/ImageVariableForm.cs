edb30deb0c0fd1e698e38a2c4c68ad764baa76bb
Marcel Baumgartner <marcel.kbkm@gmail.com>
2023-12-29 18:30:26 +0100

    Allowed empty variables

--- Moonlight/App/Models/Forms/Admin/Servers/Images/ImageVariableForm.cs
+++ Moonlight/App/Models/Forms/Admin/Servers/Images/ImageVariableForm.cs
@@ -6,6 +6,5 @@ public class ImageVariableForm
 {
     [Required(ErrorMessage = "You need to specify the key of the variable")]
     public string Key { get; set; } = "";
-    [Required(ErrorMessage = "You need to specify the value of the variable")]
     public string Value { get; set; } = "";
 }
