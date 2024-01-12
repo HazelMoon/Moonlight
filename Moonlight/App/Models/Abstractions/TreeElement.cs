be4d469a4748a9d1dddb9e415c15e64aa77848b4
Marcel Baumgartner <marcel.kbkm@gmail.com>
2024-01-12 08:21:01 +0100

    First prototype of a tree view

---
+++ Moonlight/App/Models/Abstractions/TreeElement.cs
@@ -0,0 +1,18 @@
+﻿namespace Moonlight.App.Models.Abstractions;
+
+public class TreeElement
+{
+    public TreeElement()
+    {}
+
+    public TreeElement(string name)
+    {
+        Name = name;
+    }
+    
+    public string Name { get; set; }
+    public string Icon { get; set; } = "bx bx-sm bxs-square";
+    public bool IsExpanded { get; set; } = false;
+    public Func<Task>? OnClick { get; set; }
+    public List<TreeElement> Elements { get; set; } = new();
+}
