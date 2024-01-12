5389e1a8bc70535885f8a936e9f3a443e7944587
Marcel Baumgartner <marcel.kbkm@gmail.com>
2024-01-12 11:29:03 +0100

    Improvements to tree view

--- Moonlight/App/Models/Abstractions/TreeElement.cs
+++ Moonlight/App/Models/Abstractions/TreeElement.cs
@@ -3,15 +3,23 @@
 public class TreeElement
 {
     public TreeElement()
-    {}
+    {
+        Name = "Tree Element";
+    }
 
     public TreeElement(string name)
     {
         Name = name;
     }
     
+    public TreeElement(string name, string icon)
+    {
+        Name = name;
+        Icon = icon;
+    }
+    
     public string Name { get; set; }
-    public string Icon { get; set; } = "bx bx-sm bxs-square";
+    public string Icon { get; set; } = "";
     public bool IsExpanded { get; set; } = false;
     public Func<Task>? OnClick { get; set; }
     public List<TreeElement> Elements { get; set; } = new();
